dcdc367fcea374ef18198d68f1aa04db81efc9b7
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-19 14:41:30 +0100

    completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,17 @@
 // TODO: Write your function in this file
 
+let fizzBuzz = (lower, upper) => {
+  const numsArr = [];
+  const newNumsArr = [];
+  for (let i = lower; i <= upper; i++) numsArr.push(i);
+  numsArr.map((num) => {
+    if (num % 3 === 0 && num % 5 === 0) num = 'FizzBuzz';
+    if (num % 5 === 0) num = 'Buzz';
+    if (num % 3 === 0) num = 'Fizz';
+    newNumsArr.push(num);
+  });
+  return newNumsArr;
+};
+
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz;
